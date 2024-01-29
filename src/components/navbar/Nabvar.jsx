a138454167777c1bd7a7a48734254c472ef613fc
Nico Rozo <nicolas.arenas.rozo@gmail.com>
2024-01-29 16:41:15 +0100

    navbar

---
+++ src/components/navbar/Nabvar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Nabvar = () => {
+  return (
+    <header className='navbar'>
+        <div className="navbar_logo">
+            <img src="logoTemporal.png" alt="logo" height={'100px'}/>
+        </div>
+    </header>
+  )
+}
+
+export default Nabvar
