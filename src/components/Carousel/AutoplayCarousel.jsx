517550c5164e53f3a58d85ac493477e6718f0b98
unknown <nicolas.arenas.rozo@gmail.com>
2024-02-28 16:35:29 +0100

    check point to experiment before breaking everything

--- src/components/Carousel/AutoplayCarousel.jsx
+++ src/components/Carousel/AutoplayCarousel.jsx
@@ -5,46 +5,44 @@ import CarouselItem from "./CarouselItems";
 export default function AutoplayCarousel() {
   return (
     <div className="carousel-gap">
-
- 
-    <div className="carousel-absolute">
-      <div className="carousel-container">
-        <div className="carousel-track">
-          {Object.keys(arrayIcons).map((detailKey) => {
-            return (
-              <CarouselItem
-                myIcons={arrayIcons[detailKey]}
-                key={arrayIcons[detailKey]}
-              ></CarouselItem>
-            );
-          })}
-          {Object.keys(arrayIcons).map((detailKey) => {
-            return (
-              <CarouselItem
-                myIcons={arrayIcons[detailKey]}
-                key={arrayIcons[detailKey]}
-              ></CarouselItem>
-            );
-          })}
-          {Object.keys(arrayIcons).map((detailKey) => {
-            return (
-              <CarouselItem
-                myIcons={arrayIcons[detailKey]}
-                key={arrayIcons[detailKey]}
-              ></CarouselItem>
-            );
-          })}
-          {Object.keys(arrayIcons).map((detailKey) => {
-            return (
-              <CarouselItem
-                myIcons={arrayIcons[detailKey]}
-                key={arrayIcons[detailKey]}
-              ></CarouselItem>
-            );
-          })}
+      <div className="carousel-absolute">
+        <div className="carousel-container">
+          <div className="carousel-track">
+            {Object.keys(arrayIcons).map((detailKey) => {
+              return (
+                <CarouselItem
+                  myIcons={arrayIcons[detailKey]}
+                  key={arrayIcons[detailKey]}
+                ></CarouselItem>
+              );
+            })}
+            {Object.keys(arrayIcons).map((detailKey) => {
+              return (
+                <CarouselItem
+                  myIcons={arrayIcons[detailKey]}
+                  key={arrayIcons[detailKey]}
+                ></CarouselItem>
+              );
+            })}
+            {Object.keys(arrayIcons).map((detailKey) => {
+              return (
+                <CarouselItem
+                  myIcons={arrayIcons[detailKey]}
+                  key={arrayIcons[detailKey]}
+                ></CarouselItem>
+              );
+            })}
+            {Object.keys(arrayIcons).map((detailKey) => {
+              return (
+                <CarouselItem
+                  myIcons={arrayIcons[detailKey]}
+                  key={arrayIcons[detailKey]}
+                ></CarouselItem>
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -1,7 +1,36 @@
-import {data} from "./processData.js";
-import './Process.css'
+/* eslint-disable react/prop-types */
+import { data } from "./processData.js";
+import "./Process.css";
+import { useState } from "react";
+
+function ProcessCard({card,onSelectorClick}) {
+
+  return (
+    
+            <div className="process-card" key={card.id}>
+              <div className="process-card-selector-div" onClick={onSelectorClick}>
+                <span className="process-selector-number">0{card.id}</span>
+                <h3>{card.name}</h3>
+              </div>
+              <div className="process-card-content">
+                <div className="process-card-img"></div>
+                <div className="process-card-text">
+                  <h3>{card.header}</h3>
+                  <p>{card.text}</p>
+                </div>
+              </div>
+            </div>
+  )
+}
 
 function Process() {
+  const [activeSelector,setActiveSelector] = useState(0)
+  
+  function activeWindow(item,index){
+  console.log(item)
+  console.log(index)
+}
+
   return (
     <div>
       <div>
@@ -12,14 +41,16 @@ function Process() {
         </p>
       </div>
       <div className="process-container">
-        <div className="process-rail">
-          {data.map((card)=>
-          <div className="process-card" key={card.id}>
-            
+      <div className="process-rail">
+          <ProcessCard 
+          card={data[0]}
+          onSelectorClick={activeWindow}
+          />
+          <ProcessCard 
+          card={data[1]}
+          onSelectorClick={activeWindow}
+          />
           </div>
-          )}
-
-        </div>
       </div>
     </div>
   );
