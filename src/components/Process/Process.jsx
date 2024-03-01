3f642df5a457cb34c3e51b56a260c6ebf1b8e1eb
unknown <nicolas.arenas.rozo@gmail.com>
2024-03-01 09:45:07 +0100

    Process done

--- src/components/Process/Process.jsx
+++ src/components/Process/Process.jsx
@@ -1,26 +1,9 @@
 /* eslint-disable react/prop-types */
 import { data } from "./processData.js";
 import "./Process.css";
+import ProcessCard from "./ProcessCard.jsx";
 import { useState } from "react";
 
-function ProcessCard({card,isOpen,onClick}) {
-
-  return (
-            <button className={`process-card ${isOpen ? 'active' : '' }`} key={card.id} onClick={onClick} >
-              <div className="process-card-selector-div" >
-                <span className="process-selector-number">0{card.id}</span>
-                <h3>{card.name}</h3>
-              </div>
-              <div className={`process-card-content ${isOpen ? 'active' : '' }`}>
-                <div className="process-card-img"></div>
-                <div className="process-card-text">
-                  <h3>{card.header}</h3>
-                  <p>{card.text}</p>
-                </div>
-              </div>
-            </button>
-  )
-}
 
 function Process() {
 
---
+++ src/components/Process/ProcessCard.jsx
@@ -0,0 +1,20 @@
+function ProcessCard({card,isOpen,onClick}) {
+
+    return (
+              <button className={`process-card ${isOpen ? 'active' : '' }`} key={card.id} onClick={onClick} >
+                <div className="process-card-selector-div" >
+                  <span className="process-selector-number">0{card.id}</span>
+                  <h3>{card.name}</h3>
+                </div>
+                <div className={`process-card-content ${isOpen ? 'active' : '' }`}>
+                  <div className="process-card-img"></div>
+                  <div className="process-card-text">
+                    <h3>{card.header}</h3>
+                    <p>{card.text}</p>
+                  </div>
+                </div>
+              </button>
+    )
+  }
+
+  export default ProcessCard
