4baa7f0779bf6edbfff29c3167d099d4625128e8
unknown <nicolas.arenas.rozo@gmail.com>
2024-03-04 16:02:09 +0100

    fixing spinner

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import './Footer.css'; // Importing CSS file for styling
 const Footer = () => {
   return (
     <footer className="footer">
+        <div className="footer-logo-container">Logo</div>
       <nav className="footer-nav">
         <ul className="footer-links">
           <li><a href="#">Link 1</a></li>
@@ -11,6 +12,15 @@ const Footer = () => {
           <li><a href="#">Link 4</a></li>
         </ul>
       </nav>
+      <div className="footer-policy">
+        <div className='footer-developer'>Developed by Nico Rozo</div>
+        <div className="footer-right-info">
+            <span>Privacy Policy</span>
+            <span>Terms of Service</span>
+            <span>Cookies Settings</span>
+        </div>
+
+      </div>
     </footer>
   );
 };
