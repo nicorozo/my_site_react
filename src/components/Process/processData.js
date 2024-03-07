0a3b84b775c131f6bdf9a48bc260bd9f4bdf0c75
unknown <nicolas.arenas.rozo@gmail.com>
2024-03-07 11:29:46 +0100

    navbar mobile in process

--- src/components/Process/processData.js
+++ src/components/Process/processData.js
@@ -2,30 +2,29 @@ export const data = [
   {
     id: 1,
     name: "Feature 1",
-    header: "Short heading goes here",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+    header: "Busco Diseñadores!",
+    text: "Hola, mi nombre es Nicolas, soy desarrollador web Front-End. Busco diseñadores que estén abiertos a hacer colaboraciones para paginas web con la oportunidad de ver su trabajo en vivo/online    ",
     img:'frame16.png'
   },
   {
     id: 2,
     name: "Feature 1",
-    header: "Short heading goes here",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+    header: "Apasionados, locos y creativos!",
+    text: "La idea es crear un pequeño grupo (5-10 personas) con diferentes talentos y estilos artisticos que quieran mostrar su trabajo y talento... Con libertad de creacion!",
     img:'frame16.png'
   },
   {
     id: 3,
     name: "Feature 1",
-    header: "Short heading goes here",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
-    img:'frame16.png'
+    header: "Cómo sería mi perro!",
+    text: "Que estás ampliando tu portafolio de diseño UX/UI para subirlo a Behance... Ok, porque no además hacerlo parte de una página web real y que te dé crédito (link a tu portafolio), una ventana más para mostrar tu trabajo",    img:'frame16.png'
   },
   {
     id: 4
     ,
     name: "Feature 1",
-    header: "Short heading goes here",
-    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat.",
+    header: "Me interesa pero soy timid@",
+    text: "Si leiste todo eso y aun asi te interesa, enviame una muestra de tu trabajo al DM =)",
     img:'frame16.png'
   },
 ];
