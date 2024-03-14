02d5c2057400468bc5533441ab629310100a74f4
unknown <nicolas.arenas.rozo@gmail.com>
2024-03-14 15:54:34 +0100

    mobile commit

--- src/components/Process/ProcessCard.jsx
+++ src/components/Process/ProcessCard.jsx
@@ -1,24 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 
-function ProcessCard({card,isOpen,onClick}) {
+function ProcessCard({ card, isOpen, onClick }) {
+  return (
+    <button
+      className={`process-card ${isOpen ? "active" : ""} `}
+      key={card.id}
+      onClick={onClick}
+    >
+      <div className="process-card-selector-div">
+        <span className="process-selector-number">0{card.id}</span>
+        <h3>{card.name}</h3>
+      </div>
+      <div className={`process-card-content ${isOpen ? "active" : ""}`}>
+        <img src={card.img} className="process-card-img" />
+        <div className="process-card-text">
+          <h3>{card.header}</h3>
+          <p>{card.text}</p>
+        </div>
+      </div>
+    </button>
+  );
+}
 
-
-    return (
-              <button className={`process-card ${isOpen ? 'active' : '' } `} 
-              key={card.id} onClick={onClick} >
-                <div className="process-card-selector-div" >
-                  <span className="process-selector-number">0{card.id}</span>
-                  <h3>{card.name}</h3>
-                </div>
-                <div className={`process-card-content ${isOpen ? 'active' : '' }`}>
-                  <img src={card.img} className="process-card-img"/>
-                  <div className="process-card-text">
-                    <h3>{card.header}</h3>
-                    <p>{card.text}</p>
-                  </div>
-                </div>
-              </button>
-    )
-  }
-
-  export default ProcessCard
+export default ProcessCard;
