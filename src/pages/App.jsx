a01914658f75d975219186fbb758bd768d5fceff
unknown <nicolas.arenas.rozo@gmail.com>
2024-03-22 11:45:51 +0100

    merging module to main

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -61,7 +61,7 @@ function App() {
       <div className="spacer"></div>
       <Services />
       <div className="spacer"></div>
-      {/*  <Billboard /> */}
+      <Billboard />
       <div className="spacer"></div>
       <Footer />
       {isModal && <Modal />}
